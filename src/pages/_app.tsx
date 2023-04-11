9c7dbde49200849dc0f96326f9bbdf59c95b2598
Timóteo <timoteostifft@gmail.com>
2023-04-10 21:37:39 -0300

    feat: create app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,34 @@
-import type { AppProps } from 'next/app'
+import React from "react";
+import type { AppProps } from "next/app";
+import { AppContainer, AppHeader } from "@/styles/pages/app";
+
+import GlobalStyle from "@/styles/global";
+
+import Image from "next/image";
+import profile from "@/assets/profile.jpg";
+
+import { HiBars3BottomLeft } from "react-icons/hi2";
+import Day from "@/components/Callendar/Day";
+import Add from "@/components/Tasks/Add";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppContainer>
+      <GlobalStyle />
+      <AppHeader>
+        <div>
+          <HiBars3BottomLeft size={28} />
+          <Image src={profile} height={80} width={80} alt="" />
+        </div>
+
+        <div>
+          <h2>To-do List</h2>
+          <Day />
+        </div>
+      </AppHeader>
+
+      <Add />
+      <Component {...pageProps} />
+    </AppContainer>
+  );
 }
---
+++ src/styles/pages/app.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  height: 100vh;
+`;
+
+export const AppHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  margin: 0 auto;
+  padding: 20px 20px 4rem 20px;
+
+  background-color: #454bb6;
+  color: white;
+
+  gap: 2rem;
+
+  > div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  > div:nth-child(1) {
+    img {
+      width: 40px;
+      height: 40px;
+      border-radius: 100%;
+      border: 1px solid white;
+      margin-left: auto;
+    }
+  }
+
+  > div:nth-child(2) {
+    button {
+      margin-left: auto;
+    }
+  }
+`;
