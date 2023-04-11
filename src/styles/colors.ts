91dc6e9bb5c7d06895aba124a529bf6b2d79a0d3
Timóteo <timoteostifft@gmail.com>
2023-04-11 00:50:12 -0300

    style: add app colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,26 @@
+const colors = {
+  white: "#FFFFFF",
+
+  purple: {
+    100: "#f0f1f9",
+    300: "#c4c6e9",
+    600: "#797dcd",
+    900: "#454bb6",
+  },
+
+  red: {
+    100: "#FAF0F0",
+    300: "#E8C3C3",
+    600: "#CC7878",
+    900: "#B54545",
+  },
+
+  green: {
+    100: "#F0F9F3",
+    300: "#C4E9CF",
+    600: "#6BC787",
+    900: "#45B566",
+  },
+};
+
+export default colors;
