f06f630860e311821398db9efbdf3d33c5433ae5
Timóteo <timoteostifft@gmail.com>
2023-04-15 19:06:37 -0300

    feat: add motion framer div to item component

--- src/components/Sidebar/Item/index.tsx
+++ src/components/Sidebar/Item/index.tsx
@@ -1,16 +1,40 @@
-import React, { ReactNode } from "react";
+import { motion } from "framer-motion";
+import React, { ReactNode, useState } from "react";
 import { ItemContainer } from "./styles";
 
 interface Props {
   name: string;
   icon: ReactNode;
+  children: React.ReactNode;
+  isExpandable?: true;
 }
 
-export default function Item({ name, icon }: Props) {
+export default function Item({
+  name,
+  icon,
+  children: Children,
+  isExpandable,
+}: Props) {
+  const [expand, setExpand] = useState(false);
+
   return (
-    <ItemContainer>
-      {icon}
-      <span>{name}</span>
+    <ItemContainer onClick={() => setExpand((prevState) => !prevState)}>
+      <div>
+        {icon}
+        <span>{name}</span>
+      </div>
+      {expand &&
+        (isExpandable ? (
+          <motion.div
+            initial={{ x: -200 }}
+            animate={{ x: 0 }}
+            transition={{ duration: 0.26 }}
+          >
+            {Children}
+          </motion.div>
+        ) : (
+          <>{Children}</>
+        ))}
     </ItemContainer>
   );
 }
--- src/components/Sidebar/Item/styles.ts
+++ src/components/Sidebar/Item/styles.ts
@@ -1,18 +1,28 @@
 import styled from "styled-components";
 
-export const ItemContainer = styled.div`
-  height: 40px;
-  width: 100%;
+interface ItemContainerProps {
+  isExpandable?: boolean;
+}
+
+export const ItemContainer = styled.div<ItemContainerProps>`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 8px 10px;
+
   border: 1px solid ${(props) => props.theme.colors[props.theme.main]["900"]};
   border-radius: 6px;
-  display: flex;
-  align-items: center;
-  padding: 0 10px;
   color: ${(props) => props.theme.colors[props.theme.main]["900"]};
   font-weight: 500;
-  cursor: pointer;
 
-  svg {
-    margin-right: 10px;
+  gap: 10px;
+
+  > div {
+    display: flex;
+    align-items: center;
+
+    svg {
+      margin-right: 8px;
+    }
   }
 `;
