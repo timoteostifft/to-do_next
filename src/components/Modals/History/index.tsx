d23944b50a2569ba392d97e32ba0640b192954fc
Timóteo <timoteostifft@gmail.com>
2023-04-15 19:01:12 -0300

    feat: add modal base component

---
+++ src/components/Modals/History/index.tsx
@@ -0,0 +1,12 @@
+import { ModalContainer } from "../styles";
+import { HistoryContainer } from "./styles";
+
+export default function History() {
+  return (
+    <ModalContainer>
+      <HistoryContainer>
+        <h3>Hello World!</h3>
+      </HistoryContainer>
+    </ModalContainer>
+  );
+}
